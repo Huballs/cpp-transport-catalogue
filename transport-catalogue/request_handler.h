213f66e9d949cf7326f5b5e8eb7777030cabc7c1
Huballs <ballshugo@hotmail.com>
2023-06-28 11:31:10 +0300

    modified:   transport-catalogue/request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -11,12 +11,13 @@
 
 namespace TC {
 
+#define READER_TYPENAMES typename Array, typename Dict, typename Node
+
 class RequestHandler {
     
     public:
 
         struct base_request_t {
-
             std::string_view type;
             std::string_view name;
 
@@ -27,24 +28,23 @@ class RequestHandler {
             double longitude;
 
             std::unordered_map<std::string_view, uint32_t> road_distances;
-
         };
 
         struct stat_bus_t{
-
             double curvature;
             uint32_t length;
             int stop_count;
             int unique_stop_count;
-
         };
 
         struct stat_stop_t{
-
             const std::set<std::string_view>& buses;
-            
         };
-        
+
+        struct routing_settings_t{
+            int bus_velocity;   // km/h
+            int bus_wait_time;  // minutes
+        };
         
         RequestHandler(TransportCatalogue& db, Renderer& renderer) : db_(db), renderer_(renderer){};
 
@@ -53,9 +53,6 @@ class RequestHandler {
 
         template <typename Array, typename Dict, typename Node, typename OutputBuilder>
         void ReadStatRequests(std::ostream& output, Reader<Array, Dict, Node>& reader, OutputBuilder);
-
-        template <typename Array, typename Dict, typename Node>
-        map_settings_t ReadMapRenderSettings(Reader<Array, Dict, Node>& reader);
 
         /* stores add request for later fulfillment */
         void QueueAddRequest(base_request_t& request);
@@ -81,6 +78,14 @@ class RequestHandler {
 
         TransportCatalogue& db_;
         Renderer& renderer_;
+
+        //using reader_typenames = typename Array, typename Dict, typename Node;
+
+        template <typename Array, typename Dict, typename Node>
+        map_settings_t ReadMapRenderSettings(Reader<Array, Dict, Node>& reader);
+
+        template <typename Array, typename Dict, typename Node>
+        routing_settings_t ReadRoutingSettings(Reader<Array, Dict, Node>& reader);
 
         template <typename OutputBuilder>
         typename OutputBuilder::Node_t BuildBusStatNode(int id, const stat_bus_t& bus_stat);
@@ -167,6 +172,92 @@ void RequestHandler::ReadRequests(std::ostream& output, Reader<Array, Dict, Node
     ReadStatRequests(output, reader, OutputBuilder{});
 }
 
+template <typename Array, typename Dict, typename Node, typename OutputBuilder>
+void RequestHandler::ReadStatRequests(std::ostream& output, Reader<Array, Dict, Node>& reader, OutputBuilder){
+
+    using namespace std::string_literals;
+
+    const auto request_nodes = reader.GetRequestNodesAsArray("stat_requests");
+
+    typename OutputBuilder::Array_t array_output;
+
+    for(const auto& request_node : request_nodes){
+
+        typename OutputBuilder::Node_t request_output;
+
+        int request_id = reader.GetFieldAsInt(request_node, "id");
+
+        if(reader.GetFieldAsString(request_node, "type") == "Bus"){
+
+            const auto& name = reader.GetFieldAsString(request_node, "name");
+
+            if (const auto& bus_stat = GetBusStat(name)){
+                request_output = BuildBusStatNode<OutputBuilder>(request_id, *bus_stat);
+            } else {
+                request_output = BuildNotFoundNode<OutputBuilder>(request_id);
+            }
+        } else 
+        if (reader.GetFieldAsString(request_node, "type") == "Stop"){
+
+            const auto& name = reader.GetFieldAsString(request_node, "name");
+
+            if (const auto& stop_stat = GetStopStat(name)){
+                request_output = BuildStopStatNode<OutputBuilder>(request_id, *stop_stat);
+            } else {
+                request_output = BuildNotFoundNode<OutputBuilder>(request_id);
+            }
+        } else 
+        if (reader.GetFieldAsString(request_node, "type") == "Map"){
+            auto map_renderer_settings = ReadMapRenderSettings(reader);
+            request_output = BuildMapStatNode<OutputBuilder>(request_id, map_renderer_settings);
+        }
+
+        array_output.push_back(request_output);
+
+    }
+
+    OutputBuilder{}.Value(array_output).Print(output);
+}
+
+template <typename Array, typename Dict, typename Node>
+routing_settings_t RequestHandler::ReadRoutingSettings(Reader<Array, Dict, Node>& reader){
+    return {1,1};
+}
+
+template <typename Array, typename Dict, typename Node>
+map_settings_t RequestHandler::ReadMapRenderSettings(Reader<Array, Dict, Node>& reader) {
+
+    const auto settings_map = reader.GetRequestNodesAsMap("render_settings");
+
+    map_settings_t settings;
+
+    settings.width = reader.GetFieldAsDouble(settings_map, "width");
+    settings.height = reader.GetFieldAsDouble(settings_map, "height");
+    settings.padding = reader.GetFieldAsDouble(settings_map, "padding");
+    settings.line_width = reader.GetFieldAsDouble(settings_map, "line_width");
+    settings.stop_radius = reader.GetFieldAsDouble(settings_map, "stop_radius");
+    settings.bus_label_font_size = reader.GetFieldAsInt(settings_map, "bus_label_font_size");
+    settings.stop_label_font_size = reader.GetFieldAsInt(settings_map, "stop_label_font_size");
+    settings.underlayer_width = reader.GetFieldAsDouble(settings_map, "underlayer_width");
+
+    settings.bus_label_offset = {reader.GetFieldAsArrayNodes(settings_map, "bus_label_offset")[0].AsDouble(),
+                                    reader.GetFieldAsArrayNodes(settings_map, "bus_label_offset")[1].AsDouble()};
+    
+    settings.stop_label_offset = {reader.GetFieldAsArrayNodes(settings_map, "stop_label_offset")[0].AsDouble(),
+                                    reader.GetFieldAsArrayNodes(settings_map, "stop_label_offset")[1].AsDouble()};
+    
+
+    settings.underlayer_color = detail::NodeToSvgColor(reader.GetFieldAsNode(settings_map, "underlayer_color"));
+
+    const auto color_palette = reader.GetFieldAsArrayNodes(settings_map,"color_palette");
+
+    for(const auto& color_node : color_palette){
+        settings.color_palette.push_back(detail::NodeToSvgColor(color_node));
+    }
+
+    return settings;
+}
+
 template <typename OutputBuilder>
 typename OutputBuilder::Node_t RequestHandler::BuildBusStatNode(int id, const stat_bus_t& bus_stat){
     return OutputBuilder{}.StartDict()
@@ -213,86 +304,5 @@ typename OutputBuilder::Node_t RequestHandler::BuildNotFoundNode(int id){
             .Key("error_message").Value("not found")
             .EndDict().Build();
 }
-
-template <typename Array, typename Dict, typename Node, typename OutputBuilder>
-void RequestHandler::ReadStatRequests(std::ostream& output, Reader<Array, Dict, Node>& reader, OutputBuilder){
-
-        using namespace std::string_literals;
-
-        const auto request_nodes = reader.GetRequestNodesAsArray("stat_requests");
-
-        typename OutputBuilder::Array_t array_output;
-
-        for(const auto& request_node : request_nodes){
-
-            typename OutputBuilder::Node_t request_output;
-
-            int request_id = reader.GetFieldAsInt(request_node, "id");
-
-            if(reader.GetFieldAsString(request_node, "type") == "Bus"){
-
-                const auto& name = reader.GetFieldAsString(request_node, "name");
-
-                if (const auto& bus_stat = GetBusStat(name)){
-                    request_output = BuildBusStatNode<OutputBuilder>(request_id, *bus_stat);
-                } else {
-                    request_output = BuildNotFoundNode<OutputBuilder>(request_id);
-                }
-            } else 
-            if (reader.GetFieldAsString(request_node, "type") == "Stop"){
-
-                const auto& name = reader.GetFieldAsString(request_node, "name");
-
-                if (const auto& stop_stat = GetStopStat(name)){
-                    request_output = BuildStopStatNode<OutputBuilder>(request_id, *stop_stat);
-                } else {
-                    request_output = BuildNotFoundNode<OutputBuilder>(request_id);
-                }
-            } else 
-            if (reader.GetFieldAsString(request_node, "type") == "Map"){
-                auto map_renderer_settings = ReadMapRenderSettings(reader);
-                request_output = BuildMapStatNode<OutputBuilder>(request_id, map_renderer_settings);
-            }
-
-            array_output.push_back(request_output);
-
-        }
-
-        OutputBuilder{}.Value(array_output).Print(output);
-    }
-
-template <typename Array, typename Dict, typename Node>
-map_settings_t RequestHandler::ReadMapRenderSettings(Reader<Array, Dict, Node>& reader) {
-
-        const auto settings_map = reader.GetRequestNodesAsMap("render_settings");
-
-        map_settings_t settings;
-
-        settings.width = reader.GetFieldAsDouble(settings_map, "width");
-        settings.height = reader.GetFieldAsDouble(settings_map, "height");
-        settings.padding = reader.GetFieldAsDouble(settings_map, "padding");
-        settings.line_width = reader.GetFieldAsDouble(settings_map, "line_width");
-        settings.stop_radius = reader.GetFieldAsDouble(settings_map, "stop_radius");
-        settings.bus_label_font_size = reader.GetFieldAsInt(settings_map, "bus_label_font_size");
-        settings.stop_label_font_size = reader.GetFieldAsInt(settings_map, "stop_label_font_size");
-        settings.underlayer_width = reader.GetFieldAsDouble(settings_map, "underlayer_width");
-
-        settings.bus_label_offset = {reader.GetFieldAsArrayNodes(settings_map, "bus_label_offset")[0].AsDouble(),
-                                     reader.GetFieldAsArrayNodes(settings_map, "bus_label_offset")[1].AsDouble()};
-        
-        settings.stop_label_offset = {reader.GetFieldAsArrayNodes(settings_map, "stop_label_offset")[0].AsDouble(),
-                                     reader.GetFieldAsArrayNodes(settings_map, "stop_label_offset")[1].AsDouble()};
-       
-
-        settings.underlayer_color = detail::NodeToSvgColor(reader.GetFieldAsNode(settings_map, "underlayer_color"));
-
-        const auto color_palette = reader.GetFieldAsArrayNodes(settings_map,"color_palette");
-
-        for(const auto& color_node : color_palette){
-            settings.color_palette.push_back(detail::NodeToSvgColor(color_node));
-        }
-
-        return settings;
-    }
 
 } // namespace TC
