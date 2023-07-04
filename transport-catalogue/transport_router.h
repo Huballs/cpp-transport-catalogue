0a36cdadd2f7f0557050bb04edcbf5bbfc042925
huballs <ballshugo@yandex.ru>
2023-07-04 09:14:46 +0300

    modified:   transport-catalogue/transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -14,9 +14,7 @@ public:
 
         graph_ = std::make_unique<graph::DirectedWeightedGraph<Weight>>(catalogue.GetStops().size()*10);
 
-        size_t bus_wait_distance_ = (double)settings_.bus_wait_time / 60 * settings_.bus_velocity *1000 / 2; // bus wait time converted to distance
-
-        //std::map<std::pair<size_t, size_t>, std::vector<const Bus*>> stops_to_buses;
+        size_t bus_wait_distance_ = 1.0 * settings_.bus_wait_time / 60 * settings_.bus_velocity *1000 / 2; // bus wait time converted to distance
 
         size_t bus_stop_id = catalogue.GetStops().size();
 
@@ -28,27 +26,22 @@ public:
 
             for(auto stop = stops.begin(); next_stop != stops.end(); std::advance(next_stop, 1)){
 
-                //graph_->AddEdge({(*stop)->GetIndex(), (*next_stop)->GetIndex(), catalogue.GetDistance(*stop, *next_stop)});
+                size_t actual_stop_id = (*stop)->GetIndex();
+                size_t actual_next_stop_id = (*next_stop)->GetIndex();
 
-                weight w{catalogue.GetDistance(*stop, *next_stop), bus.GetName()};
+                weight w{catalogue.GetDistance(*stop, *next_stop), false, bus.GetName(), actual_stop_id, actual_next_stop_id};
 
                 graph_->AddEdge({bus_stop_id, bus_stop_id+1, w});
 
-                std::cout << "edge from: " << bus_stop_id << " to: " << bus_stop_id+1 << " dist: " << w.distance << std::endl; 
-
-                size_t actual_stop_id = (*stop)->GetIndex();
-
-                graph_->AddEdge({actual_stop_id, bus_stop_id, bus_wait_distance_});
-                graph_->AddEdge({bus_stop_id, actual_stop_id, bus_wait_distance_});
-
-                std::cout << "intercon : " << actual_stop_id << " to: " << bus_stop_id << std::endl; 
+                graph_->AddEdge({actual_stop_id, bus_stop_id, {bus_wait_distance_, true, {}, actual_stop_id, actual_stop_id}});
+                graph_->AddEdge({bus_stop_id, actual_stop_id, {bus_wait_distance_, true, {},  actual_stop_id, actual_stop_id}});
                 
                 stop = next_stop;
                 ++bus_stop_id;
             }
 
-            graph_->AddEdge({(stops.back())->GetIndex(), bus_stop_id, bus_wait_distance_});
-            graph_->AddEdge({bus_stop_id, (stops.back())->GetIndex(), bus_wait_distance_});
+            graph_->AddEdge({(stops.back())->GetIndex(), bus_stop_id, {bus_wait_distance_, true, {}, (stops.back())->GetIndex()}});
+            graph_->AddEdge({bus_stop_id, (stops.back())->GetIndex(), {bus_wait_distance_, true, {}, (stops.back())->GetIndex()}});
 
             size_t bus_stop_id_go_back = bus_stop_id;
 
@@ -56,12 +49,13 @@ public:
 
             if(!bus.IsCircular()) {
                 auto rnext_stop = std::next(stops.rbegin());
-                //--bus_stop_id;
+
                 for(auto stop = stops.rbegin(); rnext_stop != stops.rend(); std::advance(rnext_stop, 1)){
 
-                    //graph_->AddEdge({(*stop)->GetIndex(), (*rnext_stop)->GetIndex(), catalogue.GetDistance(*stop, *rnext_stop)});
+                    size_t actual_stop_id = (*stop)->GetIndex();
+                    size_t actual_next_stop_id = (*rnext_stop)->GetIndex();
 
-                    weight w{catalogue.GetDistance(*stop, *rnext_stop), bus.GetName()};
+                    weight w{catalogue.GetDistance(*stop, *rnext_stop),false ,bus.GetName(), actual_stop_id, actual_next_stop_id};
 
                     graph_->AddEdge({bus_stop_id_go_back, bus_stop_id_go_back-1, w});
 
@@ -70,59 +64,94 @@ public:
                 }
             }
         }
-/*
-        for (const auto& [stops, distance] : catalogue.GetStopsDistances()){
-
-            Weight w{catalogue.GetDistance(stops.first, stops.second)
-                   , FindConnectingBuses(stops.first, stops.second)};
-
-            graph_->AddEdge({stops.first->GetIndex(), stops.second->GetIndex(), w});
-        }*/
         
         router_ = std::make_unique<graph::Router<Weight>>(*graph_);
     }
 
-    void Route(std::string_view from, std::string_view to){
+    enum class Route_t{
+        WAIT,
+        BUS,
+    };
+
+    struct RouteLine{
+        Route_t type;
+        std::string_view name;
+        double time;
+    };
+
+    struct Route{
+        std::vector<RouteLine> lines;
+        double total_time;
+    };
+
+    Route Route(std::string_view from, std::string_view to){
         size_t index_from = catalogue_.GetStop(from)->GetIndex(); 
         size_t index_to = catalogue_.GetStop(to)->GetIndex();
 
         auto info = router_->BuildRoute(index_from, index_to);
 
-        for(const auto& edgeID : info->edges){
-            //index_from = graph_->GetEdge(edgeID).from;
-            //index_to = graph_->GetEdge(edgeID).to;
-            //std::cout << catalogue_.GetStopByIndex(index_from)->GetName() << " - " 
-            //          << catalogue_.GetStopByIndex(index_to)->GetName() << " - " 
-            std::cout << graph_->GetEdge(edgeID).weight.bus << " - "
-                      << graph_->GetEdge(edgeID).weight.distance << " - "
-                      << (double)graph_->GetEdge(edgeID).weight.distance / settings_.bus_velocity /1000 *60
-                      << std::endl;
+        std::vector<RouteLine> route_lines;
+
+        size_t from_stop = graph_->GetEdge(info->edges[0]).weight.from;
+
+        route_lines.push_back({Route_t::WAIT
+                        ,catalogue_.GetStopByIndex(from_stop)->GetName()
+                        ,static_cast<double>(settings_.bus_wait_time)
+                        });
+
+        for(auto begin = ++(info->edges.begin()); begin < info->edges.end(); ++begin ){
+            
+            auto edgeID = *begin;
+
+            RouteLine line;
+
+            from_stop = graph_->GetEdge(edgeID).weight.from;
+
+            if(graph_->GetEdge(edgeID).weight.is_waiting){
+                line.type = Route_t::WAIT;
+                line.time = settings_.bus_wait_time;
+                line.name = catalogue_.GetStopByIndex(from_stop)->GetName();
+                ++begin; // there are 2 wait times in the graph, remove one
+            } else {
+                line.type = Route_t::BUS;
+                line.time = DistanceToTime(graph_->GetEdge(edgeID).weight.distance);
+                line.name = graph_->GetEdge(edgeID).weight.bus;
+            }
+
+            route_lines.push_back(std::move(line));
+
         }
-        std::cout << "total: " 
-                  <<  info->weight.distance << " - " 
-                  << (double)info->weight.distance  / settings_.bus_velocity /1000 *60
-                  << std::endl;
-        std::cout << std::endl;
+
+        route_lines.erase(--route_lines.end());
+
+        double total_time = DistanceToTime(info->weight.distance);
+
+        for(auto& r : route_lines){
+            if(r.type == Route_t::WAIT){
+                std::cout << "wait: " << r.name << " t: " << r.time;
+            } else {
+                std::cout << "bus:  " << r.name << " t: " << r.time;
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "total: " << total_time << std::endl << std::endl;
+
+        return {route_lines, total_time};
+
+    }
+
+    inline double DistanceToTime(size_t distance){
+        return 1.0 * distance  / settings_.bus_velocity /1000 *60;
     }
 
 private:
 
-    std::vector<std::string_view> FindConnectingBuses(Stop* from, Stop* to){
-        std::vector<std::string_view> result;
-        for(const auto& bus : from->GetBuses()){
-            const auto stops = catalogue_.GetBus(bus)->GetStops();
-            auto it = std::find(stops.begin(), stops.end(), from);
-            if(it != stops.end()){
-                if(*(it+1) == to)
-                    result.push_back(bus);
-            }
-        }
-        return result;
-    }
-
     struct weight {
         size_t distance;
+        bool is_waiting;
         std::string_view bus;
+        size_t from;
+        size_t to;
 
         bool operator>(const weight& other) const {
             return distance > other.distance;
